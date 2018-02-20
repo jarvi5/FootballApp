ebcdebce71ba1ae45b7edcefd586cc5941b22829
Javier Camacho <javicamdo@gmail.com>
2018-02-20 10:34:35 -0400

    show leagues android

---
+++ Droid/LeagueDetailActivity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+
+namespace FootballApp.Droid
+{
+    [Activity(Label = "League Detail")]
+    public class LeagueDetailActivity : ListActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Title = Intent.GetStringExtra("league");
+        }
+    }
+}
---
+++ Droid/LeaguesActivity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using FootballApp.Data;
+
+namespace FootballApp.Droid
+{
+    [Activity(Label = "Leagues", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class LeaguesActivity : ListActivity
+    {
+        DataManager dataManager = new DataManager();
+        IList<League> leagues;
+
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            leagues = (IList<League>)await dataManager.GetAllLeagues();
+            ListAdapter = new LeaguesListAdapter(this,leagues);
+        }
+
+        protected override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            base.OnListItemClick(l, v, position, id);
+            var intent = new Intent(this, typeof(LeagueDetailActivity));
+            intent.PutExtra("league", leagues[position].league);
+            StartActivity(intent);
+        }
+    }
+}
+
