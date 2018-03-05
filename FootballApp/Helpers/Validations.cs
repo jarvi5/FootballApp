bf083f48b880d20b6ffe6dfc3825d2cdb31691c9
Javier Camacho <javicamdo@gmail.com>
2018-03-05 09:54:44 -0400

    login shared

---
+++ FootballApp/Helpers/Validations.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FootballApp.Helpers
+{
+    public static class Validations
+    {
+        public static bool IsValidEmail(string email)
+        {
+            return Regex.Match(email, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").Success;
+        }
+    }
+}
